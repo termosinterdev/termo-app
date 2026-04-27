d5682f85d2b89786d349e5058fc76d58afe1e6e6
termosinterdev <l.ferreira1975@hotmail.com>
2026-04-27 08:20:24 -0300

    http://186.209.91.139:1337/admin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const Footer: React.FC<FooterProps> = ({ navigate }) => {
               <li><button onClick={() => navigate(PageRoute.CATALOG)} className="hover:text-white transition-colors">Catálogo de Produtos</button></li>
               <li className="pt-2">
                 <a 
-                  href="https://termosinter.ind.br/api/admin/"
+                  href="http://186.209.91.139:1337/admin"
                   target="_blank" 
                   rel="noopener noreferrer"
                   className="text-gray-700 hover:text-termo-yellow transition-colors"
